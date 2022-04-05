42f6ae31ab8cb841074da0a0bd1a7927c87a13be
IsaiasVenegas <isaias.venegas@ug.uchile.cl>
2022-04-05 19:16:26 +0200

    Add countries as buttons in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,10 +1,30 @@
 import "./Sidebar.css";
+import { useEffect, useState } from "react";
+
+type Country = {
+  name: string;
+  count?: number;
+};
 
 export const Sidebar = () => {
+  const [countries, setCountries] = useState<Country[] | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/countries")
+      .then((response) => response.json())
+      .then(setCountries);
+  }, []);
+
   return (
     <div id="sidebar">
       <h2>Cities App</h2>
-      <div>TODO...</div>
+      <div>
+        {countries?.map((country, index) => (
+          <button key={index} title={`Filter cities of ${country.name}`}>
+            {country.name} ({country.count})
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
