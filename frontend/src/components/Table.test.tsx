dbfd2a4a05370ed4ca925ff13fabe7cad4d0cb0e
IsaiasVenegas <isaias.venegas@ug.uchile.cl>
2022-04-05 20:53:58 +0200

    Add display test

---
+++ frontend/src/components/Table.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { Table } from "./Table";
+
+test("renders title headers", () => {
+  render(<Table />);
+  const firstHeader = screen.getByText("City");
+  const secondHeader = screen.getByText("Country");
+  const thirdHeader = screen.getByText("Sub-country");
+  const fourthHeader = screen.getByText("Website");
+  expect(firstHeader).toBeInTheDocument();
+  expect(secondHeader).toBeInTheDocument();
+  expect(thirdHeader).toBeInTheDocument();
+  expect(fourthHeader).toBeInTheDocument();
+});
